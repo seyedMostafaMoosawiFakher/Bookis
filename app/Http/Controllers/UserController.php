de018cb090ae53e2ab523c1829c542340375a5ca
seyedMostafaMoosawiFakher <smmf1363@yahoo.com>
2021-02-15 14:12:06 +0330

    the first initial

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+//my
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+
+//        $master = Role::create(['name' => 'master']);
+
+//        $writer = Role::create(['name' => 'writer']);
+
+//        $member = Role::create(['name' => 'member']);
+
+//        $permission = Permission::create(['name' => 'create review']);
+
+
+         $user = User::find(1);
+         $permission = Permission::find(1);
+
+//        $user->givePermissionTo($permission);
+//        dd($permission->assignRole($user));
+
+        $userRoles = User::role('master')->get();
+        dd($userRoles);
+
+
+
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+//my
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// model --> Authenticatable
+class User extends Authenticatable
+{
+    use HasFactory;
+
+    //my
+    use HasRoles;
+
+
+
+}
